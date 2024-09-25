6b6f2b66e4057edd7fdb738a979b1cbd6e067fa6
Halfdan Rydbeck <hrydbeck@gmail.com>
2024-09-25 12:01:36 +0200

    seqtk called without path

--- subscripts/echo/echo_setq_downsample_fastq_instrtmp.sh
+++ subscripts/echo/echo_setq_downsample_fastq_instrtmp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "/home/servicengs/bin/seqtk/seqtk \
+echo "seqtk \
 sample -s100 ${inst_temp_inputdir}/Execute_write_test_hal/fastq/12877_1_S3_R1_001.fastq.gz \
 10000 > ${inst_temp_inputdir}/Execute_write_test_hal/Test1_downsampling/12877_1_S3_R1_001.fastq_10k.gz"
